5cb822b0d0b4e9a625c9e161b8b25ba803cba04b
Stoyanov <stoyanov.karlovo@gmail.com>
2023-01-25 20:27:45 +0200

    fix database password

--- db/connect.php
+++ db/connect.php
@@ -1,4 +1,4 @@
 <?php
 require ($_SERVER["DOCUMENT_ROOT"] . "/educational_system/db/class/db.php");
-$db=new MyDatabase("localhost","root","1234","mydb");
+$db=new MyDatabase("localhost","root","2606mariya2606","mydb");
 ?>
