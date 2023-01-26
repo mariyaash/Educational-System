ef19f906a54b4e032e08b13fe40cdbba0efcd59b
Maria <mariqshahanska999@gmail.com>
2023-01-26 20:33:27 +0200

    WIP

--- db/connect.php
+++ db/connect.php
@@ -1,5 +1,4 @@
 <?php
 require ($_SERVER["DOCUMENT_ROOT"] . "/educational_system/db/class/db.php");
-require ("db/class/db.php");
 $db=new MyDatabase("localhost","root","2606mariya2606","mydb");
 ?>
