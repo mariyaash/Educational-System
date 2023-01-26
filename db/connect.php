d401644f4c24f2694dc6e49316d3274d58a40bba
Maria <mariqshahanska999@gmail.com>
2023-01-26 20:30:33 +0200

    WIP

--- db/connect.php
+++ db/connect.php
@@ -1,4 +1,8 @@
 <?php
+<<<<<<< Updated upstream
 require ($_SERVER["DOCUMENT_ROOT"] . "/educational_system/db/class/db.php");
+=======
+require ("db/class/db.php");
+>>>>>>> Stashed changes
 $db=new MyDatabase("localhost","root","2606mariya2606","mydb");
 ?>
